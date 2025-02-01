3f35bec7504d0d71f15121c736eaea9bfa6a5476
Jesse Holzman <jesseholzman96@gmail.com>
2025-01-31 23:03:38 -0500

    Move day 1 code to its own module.  Start working on web page.

--- src/genuary_2025/core.cljs
+++ src/genuary_2025/core.cljs
@@ -1,52 +1,5 @@
 (ns genuary-2025.core
-  (:require [quil.core :as q :include-macros true]
-            [quil.middleware :as m]))
+  (:require [genuary-2025.day01 :as day-01]))
 
-(def window-width 600)
-(def window-height 450)
-(def move-speed 13)
-
-(defn setup []
-  (q/frame-rate 15)
-  {:ticks 0})
-
-(defn update-state [state]
-  {:ticks (inc (:ticks state))})
-
-(defn draw-state [state]
-  (q/background 0x12 0x12 0x16)
-  (q/stroke-weight 1)
-  (let [focal-y (+ (/ window-height 2)
-                   (* (/ window-height 2)
-                      (q/sin (/ (:ticks state) 10))))
-        focal-x (+ (/ window-width 2)
-                   (* (/ window-width 2)
-                      (q/cos (/ (:ticks state) 10))))]
-
-    (q/stroke 0xe8 0xe6 0xe1)
-    (doseq [i (range 16)]
-      (let [y1 (/ focal-y
-                  (+ 2 i))
-            y2 (- window-height
-                  (/ (- window-height focal-y)
-                     (+ 2 i)))
-            x1 (/ focal-x
-                  (+ 2 i))
-            x2 (- window-width
-                  (/ (- window-width focal-x)
-                     (+ 2 i)))]
-        (q/line [x1 y1] [x2 y1])
-        (q/line [x1 y2] [x2 y2])
-        (q/line [x1 y1] [x1 y2])
-        (q/line [x2 y1] [x2 y2])))))
-
-(declare genuary-2025)
-(defn ^:export run-sketch []
-  (q/defsketch genuary-2025
-    :host "genuary-2025"
-    :size [window-width window-height]
-    :setup setup
-    :update update-state
-    :draw draw-state
-    :middleware [m/fun-mode]))
-
+(defn ^:export run-all []
+  (day-01/run-sketch))
---
+++ src/genuary_2025/day01.cljs
@@ -0,0 +1,54 @@
+(ns genuary-2025.day01
+  (:require [quil.core :as q :include-macros true]
+            [quil.middleware :as m]))
+
+(def window-width 600)
+(def window-height 450)
+(def move-speed 0.085)
+
+(defn setup []
+  (q/frame-rate 15)
+  {:ticks 0})
+
+(defn update-state [state]
+  {:ticks (inc (:ticks state))})
+
+(defn draw-state [state]
+  (q/background 0x12 0x12 0x16)
+  (q/stroke-weight 1)
+  (let [half-w (/ window-width 2)
+        half-h (/ window-height 2)
+        focal-y (+ half-h
+                   (* half-h
+                      (q/sin (* 3 (:ticks state) move-speed))))
+        focal-x (+ half-w
+                   (* half-w
+                      (q/cos (* 1 (:ticks state) move-speed))))]
+
+    (q/stroke 0xe8 0xe6 0xe1)
+    (doseq [i (range 16)]
+      (let [y1 (/ focal-y
+                  (+ 2 i))
+            y2 (- window-height
+                  (/ (- window-height focal-y)
+                     (+ 2 i)))
+            x1 (/ focal-x
+                  (+ 2 i))
+            x2 (- window-width
+                  (/ (- window-width focal-x)
+                     (+ 2 i)))]
+        (q/line [x1 y1] [x2 y1])
+        (q/line [x1 y2] [x2 y2])
+        (q/line [x1 y1] [x1 y2])
+        (q/line [x2 y1] [x2 y2])))))
+
+(declare day-01)
+(defn ^:export run-sketch []
+  (q/defsketch day-01
+    :host "day-01"
+    :size [window-width window-height]
+    :setup setup
+    :update update-state
+    :draw draw-state
+    :middleware [m/fun-mode]))
+
