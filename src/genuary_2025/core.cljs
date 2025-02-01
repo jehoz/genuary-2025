0a600c4b80d58964ff949763cca508405a5aaaf7
Jesse Holzman <jesseholzman96@gmail.com>
2025-01-31 20:04:48 -0500

    up-down part

--- src/genuary_2025/core.cljs
+++ src/genuary_2025/core.cljs
@@ -4,45 +4,34 @@
 
 (def window-width 600)
 (def window-height 450)
-(def move-speed 10)
+(def move-speed 13)
 
 (defn setup []
   (q/frame-rate 15)
-  (q/background 11)
-  {:hline-position {:x 300 :y 100}
-   :hline-direction {:x 1 :y 1}
-   :vline-position {:x (- window-width 200) :y (- window-height 100)}
-   :vline-direction {:x -1 :y -1}})
-
-(defn update-ball [position direction]
-  (let [{x :x y :y} position
-        {dx :x dy :y} direction
-        dx-next (cond (< x 0) 1
-                      (> x window-width) -1
-                      :else dx)
-        dy-next (cond (< y 0) 1
-                      (> y window-height) -1
-                      :else dy)]
-    [{:x (+ x (* move-speed dx-next))
-      :y (+ y (* move-speed dy-next))}
-     {:x dx-next :y dy-next}]))
+  {:ticks 0})
 
 (defn update-state [state]
-  (let [[h-pos h-dir] (update-ball (:hline-position state) (:hline-direction state))
-        [v-pos v-dir] (update-ball (:vline-position state) (:vline-direction state))]
-    {:hline-position h-pos
-     :hline-direction h-dir
-     :vline-position v-pos
-     :vline-direction v-dir}))
+  {:ticks (inc (:ticks state))})
 
 (defn draw-state [state]
-  (q/stroke 255)
-  (q/stroke-weight 2)
-  (let [{{h-x :x h-y :y} :hline-position
-         {v-x :x v-y :y} :vline-position} state]
-    (q/line [(- h-x 50) h-y] [(+ h-x 50) h-y])
-    (q/line [v-x (- v-y 50)] [v-x (+ v-y 50)])))
+  (q/background 13)
+  (q/stroke-weight 1)
+  (let [focal-y (+ (/ window-height 2)
+                   (* (/ window-height 2)
+                      (q/sin (/ (:ticks state) 10))))]
+    (q/stroke 127 255 0)
+    (q/line [0 focal-y] [window-width focal-y])
 
+    (q/stroke 255 255 255)
+    (doseq [i (range 16)]
+      (let [y1 (/ focal-y
+                  (+ 2 i))
+            y2 (- window-height (/ (- window-height focal-y)
+                                   (+ 2 i)))]
+        (q/line [0 y1] [window-width y1])
+        (q/line [0 y2] [window-width y2])))))
+
+(declare genuary-2025)
 (defn ^:export run-sketch []
   (q/defsketch genuary-2025
     :host "genuary-2025"
@@ -52,5 +41,3 @@
     :draw draw-state
     :middleware [m/fun-mode]))
 
-; uncomment this line to reset the sketch:
-; (run-sketch)
