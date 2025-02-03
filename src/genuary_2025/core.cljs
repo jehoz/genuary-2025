e16417c1663998145388ef3bd7f0d4e187366369
Jesse Holzman <jesseholzman96@gmail.com>
2025-02-03 00:01:05 -0500

    Display day 2 on page

--- src/genuary_2025/core.cljs
+++ src/genuary_2025/core.cljs
@@ -1,5 +1,7 @@
 (ns genuary-2025.core
-  (:require [genuary-2025.day01 :as day-01]))
+  (:require [genuary-2025.day01 :as day-01]
+            [genuary-2025.day02 :as day-02]))
 
 (defn ^:export run-all []
-  (day-01/run-sketch))
+  (day-01/run-sketch)
+  (day-02/run-sketch))
